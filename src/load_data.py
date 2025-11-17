241e4d799ab916639b5e1bac012bf52fc945071a
eliixar <alexarokakis@yahoo.ca>
2025-11-17 15:21:52 -0500

    finished adding images to sets

--- src/load_data.py
+++ src/load_data.py
@@ -5,15 +5,30 @@ def load_cifar10():
 
     transform = transforms.ToTensor()
 
-    trainset = torchvision.datasets.CIFAR10(
+    train_set = torchvision.datasets.CIFAR10(
     root="../data", train=True, download=True, transform=transform
 )
-    testset = torchvision.datasets.CIFAR10(
+    test_set = torchvision.datasets.CIFAR10(
     root="../data", train=False, download=True, transform=transform
 )
     # cifar10 has 10 classes, initialize 10 zeroes to count
     train_count = [0] * 10
     test_count = [0] * 10
 
-    train_subset = []
-    test_subset = []
+    train_data = []
+    test_data = []
+
+    # add 500 images to the train set
+    for img, label in train_set:
+        if train_count[label] < 500:
+            train_data.append(img,label)
+            train_count[label] +=1 # update counter
+
+    # do the same for the test set but with 100 images
+    for img, label in test_set:
+        if test_data[label] < 100:
+            test_data.append((img, label))
+            test_data[label] += 1
+
+    # return the train and test sets with required number of images
+    return train_data, test_data
